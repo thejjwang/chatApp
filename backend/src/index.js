76b469b78172ec379c7ba16fc71f2e29276cac7f
thejjwang <jonathanjacobwang@gmail.com>
2025-01-07 20:26:16 -0800

    restructuring indexjs

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,20 +9,20 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
 // Start the server only after the database connection is established
 const startServer = async () => {
-    try {
-      await connectDB(); // Ensure DB connection is successful
-      app.listen(PORT, () => {
-        console.log(`Server is running on PORT: ${PORT}`);
-      });
-    } catch (error) {
-      console.error("Failed to start the server:", error);
-    }
-  };
-  
-  startServer();
+  try {
+    await connectDB(); // Ensure DB connection is successful
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+  }
+};
+
+startServer();
